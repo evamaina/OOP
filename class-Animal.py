f6fdb2feeb2df1f576c5dc352be972133d312eb4
Eva <evajohnson714@gmail.com>
2017-10-22 15:06:27 +0300

    Class Dog and Cat inheriting from superclass Animal

---
+++ class-Animal.py
@@ -0,0 +1,19 @@
+"""Inheritance provides a way to share functionality between classes. 
+This similarity can be expressed by making them all inherit from a superclass Animal,
+ which contains the shared functionality"""
+class Animal: 
+  def __init__(self, name, color):
+    self.name = name
+    self.color = color
+
+class Cat(Animal):
+  def purr(self):
+    print("Purr...")
+        
+class Dog(Animal):
+  def bark(self):
+    print("Woof!")
+
+fido = Dog("Fido", "brown")
+print(fido.color)
+fido.bark()
