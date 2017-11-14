2c0d9e68dddf01e565ee7ef9a1aab1338fd48517
Eva <evajohnson714@gmail.com>
2017-11-14 14:32:33 +0300

    declaring a global variable

---
+++ global.py
@@ -0,0 +1,18 @@
+class Animal(object):
+  """Makes cute animals."""
+  is_alive = True
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+zebra = Animal("Jeffrey", 2)
+giraffe = Animal("Bruce", 1)
+panda = Animal("Chad", 7)
+
+print zebra.name, zebra.age, zebra.is_alive
+print giraffe.name, giraffe.age, giraffe.is_alive
+print panda.name, panda.age, panda.is_alive
+""" Note that each individual animal gets its own name and
+ age (since they're all initialized individually),
+ but they all have access to the member variable is_alive,
+  since they're all members of the Animal class. """
